ddc1530be5e23168c816b0a505c645b83994bfb0
Lisa Harrison <lmharrison0810@gmail.com>
2021-12-12 20:28:26 -0500

    files added

---
+++ src/components/DirectoryComponent.js
@@ -0,0 +1,63 @@
+// This is an example of a class component. It begins with the class keyword, followed by a capitalized component name that should be unique within your entire app.
+// Then, you use the JavaScript class inheritance keyword extends, followed by Component, which is the parent class provided by the React library from which you are creating this child class. 
+
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class Directory extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedCampsite: null
+        };
+    }
+
+    onCampsiteSelect(campsite) {
+        this.setState({selectedCampsite: campsite});
+    }
+
+    renderSelectedCampsite(campsite) {
+        if (campsite) {
+            return (
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        return <div />;
+    }
+
+    render() {
+        const directory = this.props.campsites.map(campsite => {
+            return (
+                <div key={campsite.id} className="col-md-5 m-1">
+                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{campsite.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            );
+        });
+
+        return (
+            <div className="container">
+                <div className="row">
+                    {directory}
+                </div>
+                <div className="row">
+                    <div className="col-md-5 m-1">
+                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Directory;
