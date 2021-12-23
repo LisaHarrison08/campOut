14f67159c04e0b38b4322d05ac2346d6e252cea8
Lisa Harrison <lmharrison0810@gmail.com>
2021-12-23 14:20:07 +0000

    comments

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -22,11 +22,37 @@ class CampsiteInfo extends Component {
         );
     }
 
+    // Comments
+
+    renderComments(comments) {
+        if (comments) {
+            return (
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {comments.map((comment) => {
+                        <div key={comment.id}></div>;
+                        <p>{comment.text}</p>;
+                        <p>{comment.author}</p>;
+                        {
+                            new Intl.DateTimeFormat('en-US', {
+                                year: 'numeric',
+                                month: 'short',
+                                day: '2-digit',
+                            }).format(new Date(Date.parse(comment.date)));
+                        }
+                    })}
+                </div>
+            );
+        }
+        return <div></div>;
+    }
+
     render() {
         if (this.props.campsite) {
             return (
                 <div className='row'>
                     {this.renderCampsite(this.props.campsite)}
+                    {this.renderComments(this.props.campsite.comments)}
                 </div>
             );
         }
