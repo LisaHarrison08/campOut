531d69d6995675116519e7e58a19dd2a208269d6
Lisa Harrison <lmharrison0810@gmail.com>
2021-12-27 23:50:50 +0000

    converted to functional component

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,33 +1,37 @@
 // This is an example of a class component. It begins with the class keyword, followed by a capitalized component name that should be unique within your entire app.
 // Then, you use the JavaScript class inheritance keyword extends, followed by Component, which is the parent class provided by the React library from which you are creating this child class. 
 
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 
+function RenderDirectoryItem({ campsite, onClick }) {
+    return (
+        <Card onClick={() => onClick(campsite.id)}>
+            <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+            <CardImgOverlay>
+                <CardTitle>{campsite.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
+}
 
-class Directory extends Component {
-    render() {
-        const directory = this.props.campsites.map(campsite => {
-            return (
-                <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.props.onClick(campsite.id)}>
-                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-                        <CardImgOverlay>
-                            <CardTitle>{campsite.name}</CardTitle>
-                        </CardImgOverlay>
-                    </Card>
-                </div>
-            );
-        });
+function Directory(props) {
 
+    const directory = props.campsites.map(campsite => {
         return (
-            <div className="container">
-                <div className="row">
-                    {directory}
-                </div>
+            <div key={campsite.id} className="col-md-5 m-1">
+                <RenderDirectoryItem campsite={campsite} onClick={props.onClick} />
             </div>
         );
-    }
+    });
+
+    return (
+        <div className="container">
+            <div className="row">
+                {directory}
+            </div>
+        </div>
+    );
 }
 
 export default Directory;
