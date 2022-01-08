6710fa5fcc61c1df42d1530714f1dda4ce11e0eb
Lisa Harrison <lmharrison0810@gmail.com>
2022-01-08 12:16:57 -0500

    modal tested and working

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -7,20 +7,21 @@ class Header extends Component {
     constructor(props) {
         super(props);
 
-        this.toggleNav = this.toggleNav.bind(this);
+        
         this.state = {
             isNavOpen: false,
             isModalOpen: false
         };
+        this.toggleNav = this.toggleNav.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleLogin = this.handleLogin.bind(this);
     }
 
     toggleNav() {
         this.setState({
             isNavOpen: !this.state.isNavOpen
         });
-        this.toggleNav = this.toggleNav.bind(this);
-        this.toggleModal = this.toggleModal.bind(this);
-        this.handleLogin = this.handleLogin.bind(this);
+        
     }
 
     toggleModal() {
