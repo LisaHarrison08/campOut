c2d64ddcb4bbe0691e211a459f0f066cce99e8de
Lisa Harrison <lmharrison0810@gmail.com>
2022-01-10 21:58:38 -0500

    Redux logger and redux thunk installed and react redux form revisited

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Button, Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, Row, Label, Col } from 'reactstrap';
 import { Control, LocalForm, Errors } from 'react-redux-form'
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 const required = val => val && val.length;
 const maxLength = len => val => !val || (val.length <= len);
@@ -148,6 +149,26 @@ function RenderComments({ comments, addComment, campsiteId }) {
 }
 
 function CampsiteInfo(props) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            </div>
+        );
+    }
     if (props.campsite) {
         return (
             <div className="container">
@@ -163,10 +184,10 @@ function CampsiteInfo(props) {
                 </div>
                 <div className='row'>
                     <RenderCampsite campsite={props.campsite} />
-                    <RenderComments 
-                    comments={props.comments} 
-                    addComment={props.addComment}
-                    campsiteId={props.campsite.id}
+                    <RenderComments
+                        comments={props.comments}
+                        addComment={props.addComment}
+                        campsiteId={props.campsite.id}
                     />
                 </div>
             </div>
